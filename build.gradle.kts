adf282eb16bc94de3731c820c8092747514a00c5
SSChen09 <sansanchen09@gmail.com>
2026-06-19 23:11:19 +0800

    fix: shadowJar not included in build task, version 2.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.ysmsync"
-version = "2.0.0"
+version = "2.0.1"
 
 repositories {
     mavenCentral()
@@ -37,4 +37,8 @@ tasks.shadowJar {
 tasks.jar {
     archiveClassifier.set("slim")
     archiveFileName.set("YSMSync-${version}-slim.jar")
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
 }
